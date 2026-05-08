052328af1eb1b1398ef08ecf46d8b0f6c21f9cd9
cookooyas <cookooyas@gmail.com>
2026-05-08 15:14:23 +0900

    initial commit

---
+++ src/main/java/com/clone/semo/SemoApplication.java
@@ -0,0 +1,13 @@
+package com.clone.semo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemoApplication.class, args);
+    }
+
+}
